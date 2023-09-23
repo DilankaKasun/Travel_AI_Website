34bed6513e0a3227caf9b2bbc8ebf0a1f46c350f
Dilanka Kasun <75939120+dkmi-dilanka@users.noreply.github.com>
2023-09-23 21:42:16 +0530

    api class up

--- test/api/config.py
+++ test/api/config.py
@@ -1,29 +1,47 @@
 class API_Data():
     header = {"X-RapidAPI-Key":"4aa76929eemsh4bcb103455c220fp120f64jsnfbeadc4698b5"}
     url = []
-    def _key(self,key,headers=header,urls=url):
+    def _key(self,key,main,sub,headers=header,urls=url):
         if key == "booking_com":
             headers["X-RapidAPI-Host"]="booking-com.p.rapidapi.com"
-            urls.append('https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates')
+            urls.append(f'https://booking-com.p.rapidapi.com/v1/{main}/{sub}')
         elif key == "Tripadvisor_com":
             headers["X-RapidAPI-Host"]="tripadvisor16.p.rapidapi.com"
-            urls.append(urls = 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchAirport')
-        elif key == "sky-scrapper":
+            urls.append(f'https://tripadvisor16.p.rapidapi.com/api/v1/{main}/{sub}')
+        elif key == "sky_scrapper":
             headers["X-RapidAPI-Host"]="sky-scrapper.p.rapidapi.com"    
-            
+            urls.append(f'https://sky-scrapper.p.rapidapi.com/api/v1//{main}/{sub}')
+
 class config(API_Data):
     def booking_com(self):
-        self._key("booking_com")
+        self._key("booking_com","data","data")
         querystring = None
+
     def tripadvisor_com(self):
-        self._key("Tripadvisor_com")
+        self._key("Tripadvisor_com","data","data")
+        querystring = None
+
+    def sky_scrapper_com(self):
+        self._key("sky_scrapper","data","data")
+        querystring = None
+
+      
 
 class  main(config):
-    def run(self):
-        self.booking_com()
-        print(self.header)
-        print(self.url)
+    def run(self,data=None):
+        if (data=="booking"):
+            self.booking_com()
+            print(self.header)
+                  # end if
         
-  
+        
+
+
+        
+
+__main__ = __name__
+
 x = main()
-x.run()
+x.run(data='booking')
+
+
--- test/api/tripadvisor.py
+++ test/api/tripadvisor.py
@@ -1,14 +1,6 @@
-import requests
+import config
 
-url = "https://booking-com.p.rapidapi.com/v1/hotels/locations"
 
-querystring = {"name":"Berlin","locale":"en-gb"}
-
-headers = {
-	"X-RapidAPI-Key": "4aa76929eemsh4bcb103455c220fp120f64jsnfbeadc4698b5",
-	"X-RapidAPI-Host": "booking-com.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
+x = config.main
+x.run(data='booking')
+print(x.header)
